d381db62b2038193363754adb12ba1aec3d90f17
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-07 09:15:25 -0300

    Fix v0.14 temporal drift tests

--- tests/test_temporal_v14.py
+++ tests/test_temporal_v14.py
@@ -4,21 +4,25 @@ from memoria_resolutiva.temporal_memory import TemporalContextMemory
 def test_temporal_memory_preserves_history_and_favors_recent_relation():
     memory = TemporalContextMemory(radius=2, decay=1.0)
     old = [
-        "rota usa ponte para atravessar",
-        "motorista escolhe ponte para atravessar",
+        "rota conecta margens do rio",
+        "ponte conecta margens do rio",
+        "cidade usa rota durante travessia",
+        "cidade usa ponte durante travessia",
     ] * 20
     new = [
-        "rota usa tunel para atravessar",
-        "motorista escolhe tunel para atravessar",
+        "rota conecta margens do rio",
+        "tunel conecta margens do rio",
+        "cidade usa rota durante travessia",
+        "cidade usa tunel durante travessia",
     ] * 40
 
     e0 = memory.add_epoch(old, label="old")
-    assert memory.similarity_at(e0, "rota", "ponte") > 0.0
+    assert memory.similarity_at(e0, "rota", "ponte") > 0.9
 
     memory.add_epoch(new, label="new")
 
     assert memory.current_similarity("rota", "tunel") > memory.current_similarity("rota", "ponte")
-    assert memory.similarity_at(e0, "rota", "ponte") > 0.0
+    assert memory.similarity_at(e0, "rota", "ponte") > 0.9
     assert memory.change_score("rota", "ponte", "tunel") > 0.0
 
 
