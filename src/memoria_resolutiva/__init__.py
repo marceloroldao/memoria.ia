7e844757f7a3123b433b0704cce19410479e2d16
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-27 00:50:56 -0300

    api(storage): expose BDR backend selection

--- src/memoria_resolutiva/__init__.py
+++ src/memoria_resolutiva/__init__.py
@@ -1,6 +1,8 @@
 from .api_v90 import MemoryConfig, ResolutiveMemoryAPI
+from .bdr_store import BDRPolicy, BDRResolutiveMemory, native_bdr_available
 from .distributed_consensus import ConsensusDecision, KnowledgeDescriptor
 from .layers import LayerSpec, layer_bits
+from .storage_backend import open_resolutive_memory, preferred_backend
 from .store import ResolutiveMemory
 
 __version__ = "0.95.0"
@@ -13,5 +15,10 @@ __all__ = [
     "LayerSpec",
     "layer_bits",
     "ResolutiveMemory",
+    "BDRPolicy",
+    "BDRResolutiveMemory",
+    "native_bdr_available",
+    "open_resolutive_memory",
+    "preferred_backend",
     "__version__",
 ]
