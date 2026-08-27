3fad2bee9801ca07290133aeb7c4acb84f3eaf23
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-27 08:57:12 -0300

    feat(api): export adaptive semantic router

--- src/memoria_resolutiva/__init__.py
+++ src/memoria_resolutiva/__init__.py
@@ -2,6 +2,7 @@ from .api_v90 import MemoryConfig, ResolutiveMemoryAPI
 from .bdr_store import BDRPolicy, BDRResolutiveMemory, native_bdr_available
 from .distributed_consensus import ConsensusDecision, KnowledgeDescriptor
 from .layers import LayerSpec, layer_bits
+from .semantic_router_v96 import AdaptiveSemanticRouterV96, SemanticRouterV96
 from .storage_backend import open_resolutive_memory, preferred_backend
 from .store import ResolutiveMemory
 
@@ -20,5 +21,7 @@ __all__ = [
     "native_bdr_available",
     "open_resolutive_memory",
     "preferred_backend",
+    "SemanticRouterV96",
+    "AdaptiveSemanticRouterV96",
     "__version__",
 ]
