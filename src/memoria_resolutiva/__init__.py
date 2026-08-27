251c1dfa24e31d26f4813e3a7b52e93f709a87d6
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-27 10:03:52 -0300

    feat(router): export hybrid and structural text routers

--- src/memoria_resolutiva/__init__.py
+++ src/memoria_resolutiva/__init__.py
@@ -1,10 +1,12 @@
 from .api_v90 import MemoryConfig, ResolutiveMemoryAPI
 from .bdr_store import BDRPolicy, BDRResolutiveMemory, native_bdr_available
 from .distributed_consensus import ConsensusDecision, KnowledgeDescriptor
+from .hybrid_text_router_v96 import HybridRoutingStats, HybridTextResolution, HybridTextRouterV96
 from .layers import LayerSpec, layer_bits
 from .semantic_router_v96 import AdaptiveRoutingStats, AdaptiveSemanticRouterV96, SemanticRouterV96, TextResolution
 from .storage_backend import open_resolutive_memory, preferred_backend
 from .store import ResolutiveMemory
+from .structural_router_v96 import StructuralResolution, StructuralSemanticRouterV96, native_structural_available
 
 __version__ = "0.95.0"
 
@@ -25,5 +27,11 @@ __all__ = [
     "AdaptiveSemanticRouterV96",
     "AdaptiveRoutingStats",
     "TextResolution",
+    "StructuralSemanticRouterV96",
+    "StructuralResolution",
+    "native_structural_available",
+    "HybridTextRouterV96",
+    "HybridTextResolution",
+    "HybridRoutingStats",
     "__version__",
 ]
